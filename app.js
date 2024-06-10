d428d8f0b28a19cc48f94cc73456e79cd2c04090
Tech Azonic <59231342+sarubasant@users.noreply.github.com>
2024-06-10 09:57:07 +0000

    commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const userModel = require('./models/user');
 
 app.set("view engine","ejs");
 app.use(express.json());
@@ -10,6 +11,23 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res)=>{
     res.render("index");
+})
+
+app.get('/read', async(req,res)=>{
+   let users = await userModel.find();
+    res.render("read",{users});
+})
+
+app.post('/create',async(req,res)=>{
+    let {name,email,image} = req.body;
+    let createdUser = await userModel.create({
+        name,
+        email,
+        image
+
+    })
+    res.redirect("/read");
+
 })
 
 app.listen(3000);
